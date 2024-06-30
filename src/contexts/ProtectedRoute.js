e7a4ef4bf6ab7086970916e773abdb4f70fc7aa2
Jonathan Yeo <joyekajie@gmail.com>
2024-07-01 01:27:53 +0800

    feat: update protected loader

--- src/contexts/ProtectedRoute.js
+++ src/contexts/ProtectedRoute.js
@@ -1,15 +1,21 @@
 import { useAuth } from './AuthContext';
 import { Navigate } from 'react-router-dom';
+import Loader from '@/components/Loader';
+import '@/App.css';
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, isLoading } = useAuth();
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className='main-loader-container'>
+        <Loader />
+      </div>
+    );
   }
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to='/login' replace />;
   }
 
   return children;
