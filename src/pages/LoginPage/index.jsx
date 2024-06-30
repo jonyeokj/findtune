a1e125c8ed2c6fb22f4f8a25473dfb8778de722b
Jonathan Yeo <joyekajie@gmail.com>
2024-06-30 22:01:48 +0800

    feat: check login url logic

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -5,6 +5,9 @@ import spotifyLogo from '@/assets/images/spotify-logo.png';
 import config from '@/config';
 
 const LoginPage = () => {
+  const loginUrl = `${config.apiBaseUrl}/login`;
+  console.log('loginUrl:', loginUrl);
+
   return (
     <div className='login-page-container'>
       <div className='login-container'>
@@ -12,7 +15,7 @@ const LoginPage = () => {
           Findtune for <img src={spotifyLogo} alt='Spotify logo' />
         </h1>
         <p>Discover new music by logging in with your Spotify account.</p>
-        <a href={`${config.API_BASE_URL}/login`} className='login-button'>
+        <a href={loginUrl} className='login-button'>
           Login with Spotify
         </a>
       </div>
